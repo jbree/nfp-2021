ed8e3294fd8c65ac64d85a6b85959fd3d26dd2a6
Joshua Breeden <jbreeden@me.com>
2021-11-15 14:32:18 +0000

    Switch to HashRouter for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from 'react-query'
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route
 } from "react-router-dom"
