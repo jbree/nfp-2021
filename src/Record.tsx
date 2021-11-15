4a4f4e641a8335a9e4e8fde967fe921dc05e8ae5
Joshua Breeden <jbreeden@me.com>
2021-11-15 00:09:05 +0000

    Add missing space when record is tied

--- src/Record.tsx
+++ src/Record.tsx
@@ -9,7 +9,7 @@ export function Record (props: RecordProps): JSX.Element {
   const r = props.record
   return (
     <>
-      {r.wins} – {r.losses}{r.ties ? `– ${r.ties}` : ''}
+      {r.wins} – {r.losses}{r.ties ? ` – ${r.ties}` : ''}
     </>
   )
 }
